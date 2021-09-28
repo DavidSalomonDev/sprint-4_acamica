5e88dfb99184cd34550b97cec718c81f18c0347b
david-cros <me@davidsalomon.dev>
2021-09-28 00:26:48 -0600

    🎉 begin sprint 4 acamica project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+		<img src="./images/logo big.svg" alt="Devs_United" />
+		<h3>Under construction</h3>
+    </div>
+  );
+}
+
+export default App;
