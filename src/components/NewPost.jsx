eea7f06e9a380b54dafb0f3a22407c90436b67ef
david-cros <me@davidsalomon.dev>
2021-10-13 01:25:16 -0600

    ✨ submit posts to firebase

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,11 +1,29 @@
 import { useState } from 'react'
+import {collection, addDoc} from 'firebase/firestore/lite'
+import {db} from '../services/firebase'
 import './styles/NewPost.css'
 
 const NewPost = () => {
-	const [charCounter, setcharCounter] = useState(0)
+	const [charCounter, setCharCounter] = useState(0)
+	const [newPost, setNewPost] = useState('')
 
-	const handleCharCounter = (e) => {
-		setcharCounter(e.target.value.length)
+	const handleInput = (e) => {
+		setCharCounter(e.target.value.length)
+		setNewPost(e.target.value)
+	}
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		if(newPost.length < 1) return
+		const postsCollection = collection(db, 'posts')
+		addDoc(postsCollection,{
+			content : newPost,
+			date: new Date(),
+			username: 'test',
+			likes: 0
+		})
+		setCharCounter(0)
+		setNewPost('')
 	}
 
 	return(
@@ -14,11 +32,12 @@ const NewPost = () => {
 				<div>
 					Profile
 				</div>
-				<form className='NewPost-form'>
+				<form className='NewPost-form' onSubmit={handleSubmit}>
 					<textarea 
-						onChange={handleCharCounter} 
+						onChange={handleInput} 
 						maxLength='200'
 						className='form-input' 
+						value={newPost}
 						placeholder="What's happening?"/>
 					<div className='form-progressbar' 
 					style={{width:charCounter/2+'%'}}></div>
