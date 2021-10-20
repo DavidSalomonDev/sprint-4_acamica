4c97875ec01d4f899fcd24cc422332eca9c9b000
david-cros <me@davidsalomon.dev>
2021-10-20 09:49:05 -0600

    💄 add sass to project

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './styles/index.css'
+import './styles/main.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import reportWebVitals from './tests/reportWebVitals'
