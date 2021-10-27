56580fc2701aee173f28a1e2f4d3c4c6dcda029f
david-cros <me@davidsalomon.dev>
2021-10-27 01:26:10 -0600

    ⚡️ add onSnapshot for realtime data

--- src/hooks/useCollections.jsx
+++ src/hooks/useCollections.jsx
@@ -1,4 +1,4 @@
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, onSnapshot } from 'firebase/firestore'
 import { useEffect, useState } from 'react'
 import sortDate from 'utils/sortDate'
 
@@ -7,11 +7,12 @@ const useCollection = (db, collectionName) => {
   const [error, setError] = useState(null)
   const [isLoading, setIsLoading] = useState(false)
 
+	/*
   const getData = async (db, collectionName) => {
     setIsLoading(true)
     try {
       const dataArray = []
-      const collectionDocs = await collection(db, collectionName)
+      const collectionDocs = collection(db, collectionName)
       const documents = await getDocs(collectionDocs)
       documents.forEach((document) => {
         let date = document.data().date ? new Date(document.data().date.toDate()) : ''
@@ -25,6 +26,27 @@ const useCollection = (db, collectionName) => {
     }
     setIsLoading(false)
   }
+  */
+
+  const getData = (db, collectionName) => {
+    setIsLoading(true)
+	const collectionDocs = collection(db, collectionName)
+
+	const unsub = onSnapshot(collectionDocs, snapshot => {
+		const dataArray = []
+		snapshot.forEach((document) => {
+			let date = document.data().date ? new Date(document.data().date.toDate()) : ''
+			dataArray.push({ ...document.data(), id: document.id, date })
+		})
+		const sortedArray = sortDate(dataArray)
+    	setData(sortedArray)
+    },error => {
+		console.error('Error from collection: ', error )
+		setError(error.code, error.message)
+	}) 
+		setIsLoading(false)
+		setTimeout(()=> unsub(),50000)
+	}
 
   useEffect(() => {
     getData(db, collectionName)
