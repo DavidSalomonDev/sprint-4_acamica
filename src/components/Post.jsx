65d589b38edbe27429b42acaddc6097e66b4ebfd
david-cros <me@davidsalomon.dev>
2021-10-26 22:43:04 -0600

    🎨 add delete button

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,6 @@
 import LikeBtn from 'components/LikeBtn'
 import postDate from 'utils/postDate'
+import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
 
 const Post = (props) => {
   const { id, content, date, isLiked, likes, username } = props
@@ -14,8 +15,9 @@ const Post = (props) => {
       <div className = 'Post__info'>
         <div className = 'Post__info--data'>
           <span className = 'Post__info--username'>{username}</span> - <span className = 'Post__info--date'>{postDate(date)}</span>
+		  <button className='Post__info--deleteBtn'><DeleteOutlineIcon /></button>
         </div>
-        <div className = 'Post__info--content'>{content}</div>
+        <p className = 'Post__info--content'>{content}</p>
         <div className = 'Post__info--likes'>
           <LikeBtn isLiked = {isLiked} />{likes}
         </div>
