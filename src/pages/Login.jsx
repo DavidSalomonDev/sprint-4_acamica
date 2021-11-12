26c91416bb9730c64abb55e3e23f334aecb0674e
salo <salo@david-salomon.com>
2021-11-12 08:56:23 -0600

    :sparkles: set persistence and add architecture

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,19 +4,20 @@ import {
   getAuth,
   signInWithPopup,
   GoogleAuthProvider,
+  browserLocalPersistence, setPersistence,
 } from 'firebase/auth'
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 
 const Login = () => {
 
   const { setUser } = useContext(UserContext)
   const auth = getAuth()
 
-  // useEffect(() => {
-  //   setPersistence(auth, browserLocalPersistence).then(() => {
-  //     setUser(auth.currentUser)
-  //   })
-  // }, [])
+  useEffect(() => {
+    setPersistence(auth, browserLocalPersistence).then(() => {
+      setUser(auth.currentUser)
+    })
+  }, [])
 
   const googleAuth = () => {
     const provider = new GoogleAuthProvider()
@@ -52,14 +53,14 @@ const Login = () => {
   }
 
   return (
-    <div className='Login'>
-      <img className='Login--logo'
-        src='https://firebasestorage.googleapis.com/v0/b/devs-united-f1635.appspot.com/o/logo%20big.svg?alt=media&token=c0c257b9-aa85-4b0e-9bff-1274c984f9e6'
-        alt='Devs_United' />
+    <div className = 'Login'>
+      <img className = 'Login--logo'
+           src = 'https://firebasestorage.googleapis.com/v0/b/devs-united-f1635.appspot.com/o/logo%20big.svg?alt=media&token=c0c257b9-aa85-4b0e-9bff-1274c984f9e6'
+           alt = 'Devs_United' />
       <h2>A social network for Developers</h2>
       <p>This is a project created by David Salomón for Acamica in Sprint 4.</p>
       {/* <button className='Login__button button' onClick={googleAuth}>Login</button> */}
-      <GoogleBtn onClick={googleAuth} />
+      <GoogleBtn onClick = {googleAuth} />
     </div>
   )
 }
