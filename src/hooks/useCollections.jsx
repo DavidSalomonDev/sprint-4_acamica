00592107e679b6333bb6ea2d20edf665523c6d7d
salo <salo@david-salomon.com>
2021-11-16 18:30:13 -0600

    :bug: reformat susbcription

--- src/hooks/useCollections.jsx
+++ src/hooks/useCollections.jsx
@@ -28,27 +28,30 @@ const useCollection = (db, collectionName) => {
   }
   */
 
-  const getData = async (db, collectionName) => {
-    setIsLoading(true)
-    const collectionDocs = collection(db, collectionName)
-
-    const unsub = onSnapshot(collectionDocs, snapshot => {
-      const dataArray = []
-      snapshot.forEach((document) => {
-        let date = document.data().date ? new Date(document.data().date.toDate()) : ''
-        dataArray.push({ ...document.data(), id: document.id, date })
-      })
-      const sortedArray = sortDate(dataArray)
-      setData(sortedArray)
-    }, error => {
-      setError(error.message)
-    })
-    setIsLoading(false)
-    setTimeout(() => unsub(), 50000000)
-  }
-
   useEffect(() => {
-    getData(db, collectionName)
+    try {
+      setIsLoading(true)
+      const collectionDocs = collection(db, collectionName)
+
+      const unsub = onSnapshot(collectionDocs, snapshot => {
+        const dataArray = []
+        snapshot.forEach((document) => {
+          let date = document.data().date ? new Date(document.data().date.toDate()) : ''
+          dataArray.push({ ...document.data(), id: document.id, date })
+        })
+        const sortedArray = sortDate(dataArray)
+        setData(sortedArray)
+      }, error => {
+        setError(error.message)
+      })
+      setIsLoading(false)
+      return () => {
+        unsub()
+      }
+    } catch (e) {
+      console.log(e)
+    }
+
   }, [db, collectionName])
 
   return [data, error, isLoading]
