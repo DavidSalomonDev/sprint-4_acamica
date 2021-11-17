87251322dca25a255aea1e169a15e266abfd141c
salo <salo@david-salomon.com>
2021-11-17 15:19:03 -0600

    :sparkles: add profile info

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,22 @@
 import NavbarProfile from 'components/NavbarProfile'
+import { UserContext } from 'context/User'
+import { useContext } from 'react'
 
 const Profile = () => {
+
+  const { user } = useContext(UserContext)
+
   return (
     <div className = 'Profile'>
       <NavbarProfile />
+      <div className = 'Profile__user'>
+        <div className = 'Profile__picture'>
+          <img src = {user.photoURL} alt = {user.displayName} style = {{ outline: `1rem solid ${user.color}` }} />
+        </div>
+        <h3 style = {{ backgroundColor: `${user.color}` }}>{user.displayName}</h3>
+      </div>
+
+
     </div>
   )
 }
