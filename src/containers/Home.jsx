0faa81e67fd4ce65af2fabad402bac479bbd4644
salo <salo@david-salomon.com>
2021-11-18 19:00:27 -0600

    :sparkles: add login reformat

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,18 +1,12 @@
-import { UserContext } from 'context/User'
-import Login from 'pages/Login'
 import Profile from 'pages/Profile'
-import { useContext } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Feed from 'pages/Feed'
 
 const Home = () => {
-  const { user } = useContext(UserContext)
+
   return (
     <div className = 'Home'>
       <Switch>
-        <Route path = '/' exact>
-          {!user.uid ? <Login /> : <Feed />}
-        </Route>
         <Route component = {Feed} path = '/feed' exact />
         <Route component = {Profile} path = '/profile' exact />
       </Switch>
